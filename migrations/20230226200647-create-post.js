d65ca5435518a7d0a64fc25167aa7476e9ad7287
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-26 16:20:55 -0400

    Add post model and migration

---
+++ migrations/20230226200647-create-post.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        required: true
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      profileId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+        model: 'Profiles',
+        key: 'id'
+      }
+    },
+      voterId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+        model: 'Profiles',
+        key: 'id'
+      }
+    },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  }
+};
