d82e8d2ddd5a98e80e0d19dedf19213d5c979852
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-28 14:12:24 -0400

    Stub up create comment function

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,18 @@
 const { Comment } = require('../models')
 
 async function createComment(req, res) {
-
+  try {
+    const { comment, postId } = req.body
+    const userId = req.user.id
+    const createdComment = await Comment.create({ content: comment, postId, userId })
+    res.status(201).json({
+      comment: createdComment,
+    })
+    if (!comment) return res.status(400).json({ message: 'please provide comment' })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 }
 
 module.exports = {
