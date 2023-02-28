1afb345ec9058faab561abede5f5713ca8058902
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-28 10:22:45 -0400

    Stub up show function

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,25 @@
-const { Post } = require('../models')
+const { Post, Profile } = require('../models')
+const profile = require('../models/profile')
 
 async function show(req, res) {
   const id = req.params.id
   try {
-    
-  } catch (error) {
-    
+    const post = await Post.findByPk(id, {
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'name'],
+        include: { model: Profile, as: 'profile', attributes: ['photo'] },
+      }
+    })
+    if (post) {
+      return res.status(200).json(post)
+    } else {
+      return res.status(404).json({ err: 'Post not found' })
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err })
   }
 }
 
