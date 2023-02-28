d46adf0ce99ea6c595f7c5c9f9896bf865fc3bb6
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-28 10:17:44 -0400

    Add show function

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,15 @@
 const { Post } = require('../models')
 
-async function create(req, res) {
+async function show(req, res) {
+  const id = req.params.id
   try {
-		req.body.posterId = req.user.profile.id
-    const post = await Post.create(req.body)
-    res.status(200).json(post)
+    
   } catch (error) {
-    res.status(500).json({ err: error })
+    
   }
 }
 
 module.exports = {
+  show,
   create
 }
